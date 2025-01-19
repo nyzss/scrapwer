c529f5c2c03dbbe622a2783a9253c96842215dbf
nyzs <oknakoca@gmail.com>
2025-01-19 01:58:17 +0100

    refactored code to be re-usable

--- main.ts
+++ main.ts
@@ -1,29 +1,42 @@
 import { scrape } from "jsr:@panha/scrape/";
 
-let COUNT = 0;
-
 const LIMIT = 1;
 const URL = "https://www.royalroad.com";
 const FICTION_NUMBER = "102061";
 
-const scraper = await scrape(`${URL}/fiction/${FICTION_NUMBER}`);
-
-const list = scraper
-    .href("a")
-    .filter((e) => e.startsWith("/fiction/" + FICTION_NUMBER))
-    .map((e) => `${URL}${e}`);
-
-const chapters = new Set<string>(list);
-
-console.log(chapters);
-
-for (const chapter of chapters) {
-    if (COUNT++ >= LIMIT) {
-        break;
+const scrapeChapters = async (
+    chapters: string[] | Set<string>,
+    limit: number = LIMIT
+) => {
+    let count = 0;
+    for (const chapter of chapters) {
+        if (count++ >= limit) {
+            break;
+        }
+        await getChapter(chapter);
     }
+};
 
+const getChapter = async (chapter: string) => {
     const chapScraper = await scrape(chapter);
 
     const chapterContent = chapScraper.text("div.chapter-inner");
     console.log(chapterContent.join("\n"));
-}
+};
+
+const scrapeById = async (id: string) => {
+    const scraper = await scrape(`${URL}/fiction/${id}`);
+
+    const list = scraper
+        .href("a")
+        .filter((e) => e.startsWith("/fiction/" + id))
+        .map((e) => `${URL}${e}`);
+
+    const chapters = new Set<string>(list);
+
+    console.log(chapters);
+
+    scrapeChapters(chapters);
+};
+
+scrapeById(FICTION_NUMBER);
